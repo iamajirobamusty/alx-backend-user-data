c23db7b13e697c3bf40be18ba226c1f88f5b6891
Mustapha Ajiroba <ajiroba980@gmail.com>
2024-09-03 13:58:10 +0000

    401 uauthorized

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -20,6 +20,11 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+@app.errorhandler(401)
+def unauthorized(error):
+    """Unauthorized
+    """
+    return jsonify({"error": "Unauthorized"})
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
