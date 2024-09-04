58ed9c202015a9cf5896aaa5a8bfe1de9b75d6c8
Mustapha Ajiroba <ajiroba980@gmail.com>
2024-09-04 16:48:18 +0000

    base64.b64decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@
 
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -27,3 +28,31 @@ class BasicAuth(Auth):
             return None
         value = authorization_header.split()
         return value[1]
+
+    # base64.decode
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        """A function that returns the deocded value of base64 
+            Returns:
+                    base64.decode()
+            Usage:
+                a = BasicAuth()
+
+                print(a.decode_base64_authorization_header(None))
+                print(a.decode_base64_authorization_header(89))
+                print(a.decode_base64_authorization_header("Holberton School"))
+                print(a.decode_base64_authorization_header("SG9sYmVydG9u"))
+                print(a.decode_base64_authorization_header("SG9sYmVydG9uIFNjaG9vbA=="))
+                print(a.decode_base64_authorization_header(a.extract_base64_authorization_header("Basic SG9sYmVydG9uIFNjaG9vbA==")))
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            utfdecode = base64_authorization_header.encode('utf-8')
+            b64 = base64.b64decode(utfdecode)
+            return b64.decode('utf-8')
+        except Exception:
+            return None
