0be62c0c016af0c125b6612fbdd5171bb7de6339
Mustapha Ajiroba <ajiroba980@gmail.com>
2024-09-04 15:41:24 +0000

    basic_auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python3
-"""
-Module for authentication using Basic auth
+"""Basic Authentication
 """
 
 
-from typing import TypeVar
 from api.v1.auth.auth import Auth
-import base64
-
-from models.user import User
 
 
 class BasicAuth(Auth):
-        """_summary_
-        """
-        pass
+    """A class that implement the basic form of authentication
+    """
