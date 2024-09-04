d24b0273ef3f7003daffb92935af336ab78a3314
Ajiroba Mustapha Abiodun <110599927+iamajirobamusty@users.noreply.github.com>
2024-09-04 15:24:17 +0100

    Update auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -27,10 +27,13 @@ class Auth:
         """
         if path is None:
             return True
+            
         if excluded_paths is None or excluded_paths == []:
             return True
+            
         if path in excluded_paths:
             return False
+            
         else:
             for i in excluded_paths:
                 if i.startswith(path):
