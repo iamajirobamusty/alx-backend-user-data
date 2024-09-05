e29ac5a131071eb5609ad9024b19dc594ed6f0ea
Ajiroba Mustapha Abiodun <110599927+iamajirobamusty@users.noreply.github.com>
2024-09-05 21:06:00 +0100

    Update app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -34,11 +34,14 @@ def before_request():
         excluded_list = [
             '/api/v1/status/',
             '/api/v1/unauthorized/',
-            '/api/v1/forbidden/'
+            '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
         ]
         if auth.require_auth(request.path, excluded_list):
-            if auth.authorization_header(request) is None:
+            if auth.authorization_header(request) is None :
                 abort(401, description='Unauthorized')
+            if auth.session_cookie(request) is None:
+                abort(401)
             if auth.current_user(request) is None:
                 abort(403, description='Forbidden')
 
