6175dd7cbbb49aaa19925b79d431854a736bea9e
Ajiroba Mustapha Abiodun <110599927+iamajirobamusty@users.noreply.github.com>
2024-09-05 15:34:16 +0100

    Update auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -15,7 +15,7 @@ class Auth:
             def authorization_header(self, request=None) -> str:
             def current_user(self, request=None) -> str:
     """
-    
+
     # Authentication required
     def require_auth(self, path: str, 
                      excluded_paths: List[str]) -> bool:
@@ -27,13 +27,13 @@ class Auth:
         """
         if path is None:
             return True
-            
+
         if excluded_paths is None or excluded_paths == []:
             return True
-            
+
         if path in excluded_paths:
             return False
-            
+
         else:
             for i in excluded_paths:
                 if i.startswith(path):
@@ -43,7 +43,7 @@ class Auth:
                 elif i[-1] == "*":
                     if path.startswith(i[:1]):
                         return False
-                        
+    
         return True
 
     def authorization_header(self, request=None) -> str:
