4f3ab5fe91a4219facb6ed180d11029562c138d3
Mustapha Ajiroba <ajiroba980@gmail.com>
2024-09-05 16:46:20 +0000

    Session ID for a particular session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,6 +5,7 @@
 from api.v1.auth.auth import Auth
 import uuid
 
+
 class SessionAuth(Auth):
     """A class that implements session based authentication
     """
@@ -22,3 +23,15 @@ class SessionAuth(Auth):
         session_id = uuid.uuid4()
         self.user_id_by_session_id[str(session_id)] = user_id
         return str(session_id)
+
+    def user_id_for_session_id(
+            self,
+            session_id: str = None) -> str:
+        """A function that returns a session ID"""
+        if session_id is None:
+            return None
+        if not isinstance(session_id, str):
+            return None
+
+        value = self.user_id_by_session_id.get('sessoin_id')
+        return str(value)
