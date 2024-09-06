9dbdcc208444e597212734aac80964bd3b97d192
Ajiroba Mustapha Abiodun <110599927+iamajirobamusty@users.noreply.github.com>
2024-09-06 11:35:12 +0100

    Update auth.py

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -27,13 +27,13 @@ class Auth:
         """
         if path is None:
             return True
-            
+
         if excluded_paths is None or excluded_paths == []:
             return True
-            
+
         if path in excluded_paths:
             return False
-            
+
         else:
             for i in excluded_paths:
                 if i.startswith(path):
@@ -43,7 +43,7 @@ class Auth:
                 elif i[-1] == "*":
                     if path.startswith(i[:1]):
                         return False
-                        
+         
         return True
 
     def authorization_header(self, request=None) -> str:
@@ -71,4 +71,3 @@ class Auth:
             return None
         session_name = os.getenv('SESSION_NAME')
         return request.cookies.get(session_name)
-
